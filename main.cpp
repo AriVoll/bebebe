52c450e336d2733be6078b1e86191e992ad3ffa5
AriVoll <volosh_2021@mail.ru>
2025-09-26 10:24:54 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+
+
+  std::cout << "$ ";
+
+  std::string input;
+  std::getline(std::cin, input);
+}
