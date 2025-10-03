1a26a6a485017469982a50e171684b3b4a60d84c
AriVoll <volosh_2021@mail.ru>
2025-10-03 10:02:22 +0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main() {
+   std::string input;
+   std::cout<<"vvedi stroky";
+   std::getline(std::cin,input);
+   std::cout<<"ti vvel:"<<input<<std::endl;
+
+
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+
+
+  std::cout << "$ ";
+
+  std::string input;
+  std::getline(std::cin, input);
+}
